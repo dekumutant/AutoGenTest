10fbdb05c3199d568ea159a0667028c055c9a953
Harrison Irwin <harrison.irwin@arq.group>
2023-10-26 21:12:37 +1000

    added base

---
+++ app.py
@@ -0,0 +1,54 @@
+import autogen
+import openai
+import os
+from dotenv import load_dotenv
+ 
+
+# Load the environment variables from dev.env
+load_dotenv("dev.env")
+
+config_list = [
+    {
+        'model':'gpt-4',
+        'api_key': os.getenv("API_KEY")
+    }
+]
+
+llm_config={
+    "request_timeout": 600,
+    "seed": 42,
+    "config_list": config_list,
+    "temperature": 0
+}
+
+assistant = autogen.AssistantAgent(
+    name="assistant",
+    llm_config=llm_config,
+)
+
+user_proxy = autogen.UserProxyAgent(
+    name="user_proxy",
+    human_input_mode="NEVER",
+    max_consecutive_auto_reply=5,
+    is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("terminate"),
+    code_execution_config={"work_dir": "web"},
+    llm_config=llm_config,
+    system_message="""Reply TERMINATE if the task has been solved at full satisfaction. Otherwise, reply CONTINUE, or the reason why the task is not solved yet."""
+)
+
+task = """
+What date is today? Compare the year-to-date gain for META and TESLA.
+"""
+
+user_proxy.initiate_chat(
+    assistant,
+    message=task
+)
+# task2 = """
+# Change the code in the file you just created to instead output numbers 1 to 200
+# """
+
+# user_proxy.initiate_chat(
+#     assistant,
+#     message=task2
+# )
