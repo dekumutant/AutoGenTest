279a7534e923521b1d2efc94f803881b55f7970f
Harrison Irwin <bora.horza@yahoo.com.au>
2023-10-28 11:50:32 +1000

    Dockerising and tests

---
+++ README.md
@@ -0,0 +1,5 @@
+To allow docket to work on windows.
+Download docker
+R=In admin powershell run wsl --update
+wsl -l -v
+https://www.youtube.com/watch?v=JRluDLoiHXM&t=309s explains
--- app.py
+++ app.py
@@ -26,18 +26,19 @@ assistant = autogen.AssistantAgent(
     llm_config=llm_config,
 )
 
+##to use docker code_execution_config={"work_dir": "web", "use_docker": "python:3"},
 user_proxy = autogen.UserProxyAgent(
     name="user_proxy",
     human_input_mode="NEVER",
-    max_consecutive_auto_reply=5,
+    max_consecutive_auto_reply=10,
     is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("terminate"),
-    code_execution_config={"work_dir": "web"},
+    code_execution_config={"work_dir": "web", "use_docker": "python:3"},
     llm_config=llm_config,
     system_message="""Reply TERMINATE if the task has been solved at full satisfaction. Otherwise, reply CONTINUE, or the reason why the task is not solved yet."""
 )
 
 task = """
-What date is today? Compare the year-to-date gain for META and TESLA.
+Generate code that draws a picture of hatsune miku
 """
 
 user_proxy.initiate_chat(
