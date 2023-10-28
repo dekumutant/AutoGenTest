68d97ff9a88db689bdf588965538a718d3782c20
Harrison Irwin <bora.horza@yahoo.com.au>
2023-10-28 22:10:48 +1000

    info

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Download docker
 R=In admin powershell run wsl --update
 wsl -l -v
 https://www.youtube.com/watch?v=JRluDLoiHXM&t=309s explains
+
+https://microsoft.github.io/autogen/docs/FAQ/
--- app.py
+++ app.py
@@ -12,17 +12,7 @@ config_list = autogen.config_list_from_json(
     file_location=".",
     filter_dict={
         "model": {
-            "gpt-4",
-            "gpt4",
-            "gpt-4-32k",
-            "gpt-4-32k-0314",
-            "gpt-4-32k-v0314",
-            "gpt-3.5-turbo",
             "gpt-3.5-turbo-16k",
-            "gpt-3.5-turbo-0301",
-            "chatgpt-35-turbo-0301",
-            "gpt-35-turbo-v0301",
-            "gpt",
         }
     }
 )
